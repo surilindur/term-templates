be2d55a196be65a003e18ba89d891ac1f70750d4
surilindur <16085353+surilindur@users.noreply.github.com>
2026-08-17 17:05:59 +0200

    Add batching, update tests

--- lib/TermTemplateProcessor.ts
+++ lib/TermTemplateProcessor.ts
@@ -1,8 +1,9 @@
 import type * as RDF from '@rdfjs/types';
+import MurmurHash3 from 'imurmurhash';
 import { DataFactory } from 'rdf-data-factory';
-import type { BindingsStream, IQueryEngine, QuerySourceUnidentified } from '@comunica/types';
-import { AlgebraFactory, algebraUtils } from '@comunica/utils-algebra';
-import type { Algebra } from '@traqula/algebra-transformations-1-2';
+import type { IQueryEngine, QuerySourceUnidentified } from '@comunica/types';
+import type { Algebra, AlgebraFactory } from '@traqula/algebra-transformations-1-2';
+import { algebraUtils } from '@traqula/algebra-transformations-1-2';
 import { Generator } from '@traqula/generator-sparql-1-2';
 import { Parser } from '@traqula/parser-sparql-1-2';
 import { toAlgebra, toAst } from '@traqula/algebra-sparql-1-2';
@@ -10,22 +11,42 @@ import { Eta } from 'eta';
 import { TermTemplate } from './TermTemplate';
 import type { ITermTemplateContext } from './TermTemplateContext';
 
+type QueryBatch<T> = {
+  query: string;
+  sources: string[];
+  bindings: Record<string, RDF.NamedNode | RDF.Literal>[];
+  resolutions: Record<string, ((value: T) => void)[]>;
+  rejections: Record<string, ((error: unknown) => void)[]>;
+};
+
+type QueryBuffer<T> = Record<string, QueryBatch<T>>;
+
 class TermTemplateProcessor {
   public readonly queryEngine: IQueryEngine;
-  public readonly templateSources: QuerySourceUnidentified[];
+  public readonly dataFactory: DataFactory;
+  public readonly algebraFactory: AlgebraFactory;
 
+  public readonly templateSources: QuerySourceUnidentified[];
   public readonly templates: Map<string, TermTemplate>;
+  public readonly selectBuffer: QueryBuffer<RDF.Bindings[]>;
+  public readonly askBuffer: QueryBuffer<boolean>;
+  public readonly batchWindowMilliseconds: number;
+  public readonly queryIdentifierVariable: RDF.Variable;
 
   public static readonly eta = new Eta({ useWith: true });
   public static readonly sparqlParser = new Parser();
   public static readonly sparqlGenerator = new Generator();
-  public static readonly dataFactory = new DataFactory();
-  public static readonly algebraFactory = new AlgebraFactory(TermTemplateProcessor.dataFactory);
 
   public constructor(options: ITermTemplateProcessorOptions) {
     this.queryEngine = options.queryEngine;
+    this.dataFactory = options.dataFactory;
+    this.algebraFactory = options.algebraFactory;
     this.templateSources = options.templateSources;
     this.templates = new Map();
+    this.selectBuffer = {};
+    this.askBuffer = {};
+    this.batchWindowMilliseconds = options.batchWindow;
+    this.queryIdentifierVariable = options.dataFactory.variable(options.queryIdentifierVariable);
   }
 
   /**
@@ -55,6 +76,18 @@ SELECT DISTINCT ?template ?priority ?text ?pattern ?target WHERE {
     }
   }
 
+  public static parseOperation(op: string): Algebra.Operation {
+    const parsedOperation = TermTemplateProcessor.sparqlParser.parse(op);
+    const parsedAlgebra = toAlgebra(parsedOperation);
+    return parsedAlgebra;
+  }
+
+  public static serializeOperation(operation: Algebra.Operation): string {
+    const operationAst = toAst(operation);
+    const operationString = TermTemplateProcessor.sparqlGenerator.generate(operationAst);
+    return operationString;
+  }
+
   public findApplicableTemplates(term: RDF.Term): TermTemplate[] {
     const templatesWithRelevance: { template: TermTemplate; relevance: number }[] = [];
     for (const template of this.templates.values()) {
@@ -71,16 +104,11 @@ SELECT DISTINCT ?template ?priority ?text ?pattern ?target WHERE {
     if (template) {
       const templateContext: ITermTemplateContext = {
         term,
-        queryAsk: async (query, bindings) => {
-          const queryWithBindings = TermTemplateProcessor.addBindingsToQuery(query, bindings ?? []);
-          const result = await this.queryEngine.queryBoolean(queryWithBindings, { sources: [template.target] });
-          return result;
-        },
+        queryAsk: (query, bindings) => this.askBatched(query, [template.target], bindings ?? []),
         querySelect: async (query, bindings) => {
-          const queryWithBindings = TermTemplateProcessor.addBindingsToQuery(query, bindings ?? []);
-          const result = await this.queryEngine.queryBindings(queryWithBindings, { sources: [template.target] });
-          const output = await TermTemplateProcessor.bindingsToRecords(result);
-          return output;
+          const outputBindings = await this.selectBatched(query, [template.target], bindings ?? []);
+          const outputRecords = TermTemplateProcessor.bindingsToRecords(outputBindings);
+          return outputRecords;
         },
         visualiseTerm: term => this.visualiseTerm(term)
       };
@@ -93,11 +121,11 @@ SELECT DISTINCT ?template ?priority ?text ?pattern ?target WHERE {
     return `<pre>${JSON.stringify(term, undefined, 2)}</pre>`;
   }
 
-  public static async bindingsToRecords(bindingsStream: BindingsStream): Promise<Record<string, RDF.Term>[]> {
+  public static bindingsToRecords(bindings: RDF.Bindings[]): Record<string, RDF.Term>[] {
     const output: Record<string, RDF.Term>[] = [];
-    for await (const bindings of bindingsStream) {
+    for (const mapping of bindings) {
       const record: Record<string, RDF.Term> = {};
-      for (const [key, value] of bindings) {
+      for (const [key, value] of mapping) {
         record[key.value] = value;
       }
       output.push(record);
@@ -105,36 +133,246 @@ SELECT DISTINCT ?template ?priority ?text ?pattern ?target WHERE {
     return output;
   }
 
-  public static addBindingsToQuery(query: string, bindings: Record<string, RDF.NamedNode | RDF.Literal>[]): string {
-    const parsedQuery = TermTemplateProcessor.sparqlParser.parse(query);
-    const parsedAlgebra = toAlgebra(parsedQuery);
+  public bindingsToValues(bindings: Record<string, RDF.NamedNode | RDF.Literal>[]): Algebra.Values {
+    return this.algebraFactory.createValues(
+      [...new Set(bindings.flatMap(b => Object.keys(b)))].map(s => this.dataFactory.variable(s)),
+      bindings
+    );
+  }
 
-    const operationWithValues = algebraUtils.mapOperation(parsedAlgebra, {
+  public addBindings(query: string, bindings: Record<string, RDF.NamedNode | RDF.Literal>[]): string {
+    const operation = TermTemplateProcessor.parseOperation(query);
+
+    const operationWithValues = algebraUtils.mapOperation(operation, {
       project: {
         preVisitor: () => ({ shortcut: true, copy: false }),
-        transform: (_copy, op) => TermTemplateProcessor.algebraFactory.createProject(
-          TermTemplateProcessor.algebraFactory.createJoin([
-            op.input,
-            TermTemplateProcessor.algebraFactory.createValues(
-              [...new Set(bindings.flatMap(b => Object.keys(b)))].map(s => TermTemplateProcessor.dataFactory.variable(s)),
-              bindings
-            )
-          ]),
+        transform: (_copy, op) => this.algebraFactory.createProject(
+          this.algebraFactory.createJoin([op.input, this.bindingsToValues(bindings)]),
           op.variables
         )
       }
     }) as Algebra.Operation;
 
-    const operationWithValuesAst = toAst(operationWithValues);
-    const operationWithValuesQueryString = TermTemplateProcessor.sparqlGenerator.generate(operationWithValuesAst);
+    return TermTemplateProcessor.serializeOperation(operationWithValues);
+  }
 
-    return operationWithValuesQueryString;
+  /**
+   * Generate a query identifier that takes into account also the binding values.
+   */
+  public generateQueryIdentifier(query: string, sources: string[], bindings: Record<string, RDF.NamedNode | RDF.Literal>[]): RDF.NamedNode {
+    const value = JSON.stringify({
+      query,
+      sources: sources.sort(),
+      // Ensure the binding objects always sorted identically
+      bindings: Object.fromEntries(Object.entries(bindings).sort((a, b) => a[0] < b[0] ? -1 : 1))
+    });
+    const hash = MurmurHash3(value).result().toString(16);
+    return this.dataFactory.namedNode(`urn:query:${hash}`);
+  }
+
+  /**
+   * Generate a query batch identifier, to allow grouping together identical queries to a single source.
+   */
+  public generateQueryBatchIdentifier(query: string, sources: string[], bindings: Record<string, RDF.NamedNode | RDF.Literal>[]): RDF.NamedNode {
+    const value = JSON.stringify({
+      query,
+      sources: sources.sort(),
+      // Only considers the keys, because different values could exist for the variables
+      bindings: Object.keys(bindings).sort()
+    });
+    const hash = MurmurHash3(value).result().toString(16);
+    return this.dataFactory.namedNode(`urn:batch:${hash}`);
+  }
+
+  /**
+   * Helper function to push executions into buffers based on identifiers.
+   */
+  public async executeBatched<T>(
+    query: string,
+    sources: string[],
+    bindings: Record<string, RDF.NamedNode | RDF.Literal>[],
+    buffer: QueryBuffer<T>,
+    executor: (batchIdentifier: RDF.NamedNode) => Promise<void>
+  ): Promise<T> {
+    const queryIdentifier = this.generateQueryIdentifier(query, sources, bindings);
+    const batchIdentifier = this.generateQueryBatchIdentifier(query, sources, bindings);
+
+    // Add the current query identifier to the bindings
+    bindings = bindings.map(b => ({ ...b, [this.queryIdentifierVariable.value]: queryIdentifier }));
+
+    return new Promise<T>((resolve, reject) => {
+      if (buffer[batchIdentifier.value]) {
+        if (
+          buffer[batchIdentifier.value].resolutions[queryIdentifier.value]
+          && buffer[batchIdentifier.value].rejections[queryIdentifier.value]
+        ) {
+          buffer[batchIdentifier.value].resolutions[queryIdentifier.value].push(resolve);
+          buffer[batchIdentifier.value].rejections[queryIdentifier.value].push(reject);
+        } else {
+          buffer[batchIdentifier.value].bindings.push(...bindings);
+          buffer[batchIdentifier.value].resolutions[queryIdentifier.value] = [resolve];
+          buffer[batchIdentifier.value].rejections[queryIdentifier.value] = [reject];
+        }
+      } else {
+        const selectOperation = TermTemplateProcessor.parseOperation(query);
+        const selectOperationWithQueryVariable = algebraUtils.mapOperation(selectOperation, {
+          project: {
+            preVisitor: () => ({ shortcut: true, copy: false }),
+            transform: (_copy, op) => this.algebraFactory.createProject(op.input, [...op.variables, this.queryIdentifierVariable])
+          }
+        }) as Algebra.Operation;
+
+        buffer[batchIdentifier.value] = {
+          query: TermTemplateProcessor.serializeOperation(selectOperationWithQueryVariable),
+          sources,
+          bindings,
+          resolutions: { [queryIdentifier.value]: [resolve] },
+          rejections: { [queryIdentifier.value]: [reject] }
+        };
+        setTimeout(() => executor(batchIdentifier), this.batchWindowMilliseconds);
+      }
+    });
+  }
+
+  public async executeAsk(batchIdentifier: RDF.NamedNode): Promise<void> {
+    const batch = this.askBuffer[batchIdentifier.value];
+    delete this.askBuffer[batchIdentifier.value];
+    console.log('ASK', batch);
+
+    const askOperation = TermTemplateProcessor.parseOperation(batch.query);
+
+    const selectOperation = algebraUtils.mapOperation(askOperation, {
+      ask: {
+        preVisitor: () => ({ shortcut: true, copy: false }),
+        transform: (_, op) => this.algebraFactory.createProject(
+          this.algebraFactory.createFilter(
+            this.bindingsToValues(batch.bindings),
+            this.algebraFactory.createExistenceExpression(false, op.input)
+          ),
+          [this.queryIdentifierVariable]
+        )
+      }
+    }) as Algebra.Operation;
+
+    try {
+      const queryString = TermTemplateProcessor.serializeOperation(selectOperation);
+      const bindingsArray = await this.selectAll(queryString, batch.sources);
+
+      // Report existence to their corresponding queries
+      for (const bindings of bindingsArray) {
+        const queryIdentifier = (bindings.get(this.queryIdentifierVariable) as RDF.NamedNode).value;
+        for (const resolution of batch.resolutions[queryIdentifier]) {
+          resolution(true);
+        }
+        delete batch.resolutions[queryIdentifier];
+      }
+
+      // Report non-existence to all other queries
+      for (const resolutions of Object.values(batch.resolutions)) {
+        for (const resolution of resolutions) {
+          resolution(false);
+        }
+      }
+    } catch (error: unknown) {
+      // Send the error to all queries
+      for (const rejections of Object.values(batch.rejections)) {
+        for (const rejection of rejections) {
+          rejection(error);
+        }
+      }
+      // Raise the error so it gets propagated
+      throw error;
+    }
+  }
+
+  public async selectAll(query: string, sources: string[]): Promise<RDF.Bindings[]> {
+    const bindingsStream = await this.queryEngine.queryBindings(query, { sources, lenient: true });
+    const bindingsArray = await bindingsStream.toArray();
+    return bindingsArray;
+  }
+
+  public async executeSelect(batchIdentifier: RDF.NamedNode): Promise<void> {
+    const batch = this.selectBuffer[batchIdentifier.value];
+    delete this.selectBuffer[batchIdentifier.value];
+    console.log('SELECT', batch);
+
+    try {
+      const queryWithBindings = this.addBindings(batch.query, batch.bindings);
+      const bindingsArray = await this.selectAll(queryWithBindings, batch.sources);
+      const bindingsByQuery: Record<string, RDF.Bindings[]> = {};
+
+      for (const bindings of bindingsArray) {
+        console.log('bindings', bindings);
+        const queryIdentifier = (bindings.get(this.queryIdentifierVariable) as RDF.NamedNode).value;
+        if (bindingsByQuery[queryIdentifier]) {
+          bindingsByQuery[queryIdentifier].push(bindings);
+        } else {
+          bindingsByQuery[queryIdentifier] = [bindings];
+        }
+      }
+
+      // Forward bindings to their respective resolution functions
+      for (const [identifier, bindings] of Object.entries(bindingsByQuery)) {
+        for (const resolution of batch.resolutions[identifier]) {
+          resolution(bindings);
+        }
+        delete batch.resolutions[identifier];
+      }
+
+      // Mark all remaining queries without bindings as finished
+      for (const resolutions of Object.values(batch.resolutions)) {
+        for (const resolution of resolutions) {
+          resolution([]);
+        }
+      }
+    } catch (error: unknown) {
+      // Send the error to all queries
+      for (const rejections of Object.values(batch.rejections)) {
+        for (const rejection of rejections) {
+          rejection(error);
+        }
+      }
+      // Raise the error so it gets propagated
+      throw error;
+    }
+  }
+
+  public async askBatched(query: string, sources: string[], bindings: Record<string, RDF.NamedNode | RDF.Literal>[]): Promise<boolean> {
+    return this.executeBatched<boolean>(query, sources, bindings, this.askBuffer, this.executeAsk);
+  }
+
+  public async selectBatched(query: string, sources: string[], bindings: Record<string, RDF.NamedNode | RDF.Literal>[]): Promise<RDF.Bindings[]> {
+    return this.executeBatched<RDF.Bindings[]>(query, sources, bindings, this.selectBuffer, this.executeSelect);
   }
 }
 
 interface ITermTemplateProcessorOptions {
+  /**
+   * The query engine to use for template retrieval and within the templates themselves.
+   */
   queryEngine: IQueryEngine;
+  /**
+   * The data factory to use for creating RDF terms.
+   */
+  dataFactory: DataFactory;
+  /**
+   * The algebra factory to use for creating algebra objects.
+   */
+  algebraFactory: AlgebraFactory;
+  /**
+   * The sources to retrieve templates from.
+   */
   templateSources: QuerySourceUnidentified[];
+  /**
+   * The window in milliseconds for every query batch.
+   */
+  batchWindow: number;
+  /**
+   * The variable name to use for mapping solutions back to the original query in batches.
+   * This is added to each query that is sent to the server, so ensure it does not conflict
+   * with existing variables in queries, including the queries within templates.
+   */
+  queryIdentifierVariable: string;
 }
 
 export { TermTemplateProcessor, type ITermTemplateProcessorOptions };
--- test/TermTemplateProcessor.test.ts
+++ test/TermTemplateProcessor.test.ts
@@ -1,16 +1,19 @@
 import * as RDF from '@rdfjs/types';
-import { beforeEach, describe, expect, it, vi } from 'vitest';
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
 import { ArrayIterator } from 'asynciterator';
 import { DataFactory } from 'rdf-data-factory';
-import type { BindingsStream, IQueryEngine } from '@comunica/types';
+import type { IQueryEngine } from '@comunica/types';
+import { AlgebraFactory } from '@traqula/algebra-transformations-1-2';
 import type { TermTemplate } from '../lib/TermTemplate';
 import { TermTemplateProcessor } from '../lib/TermTemplateProcessor';
+import { ITermTemplateContext } from '../lib';
 
 describe('TermTemplateProcessor', () => {
   let processor: TermTemplateProcessor;
   let queryEngine: IQueryEngine;
 
-  const DF = new DataFactory();
+  const dataFactory = new DataFactory();
+  const algebraFactory = new AlgebraFactory(dataFactory);
 
   const templateSources: string[] = [
     'urn:example:source'
@@ -20,17 +23,26 @@ describe('TermTemplateProcessor', () => {
     queryEngine = {
       queryBindings: vi.fn().mockImplementation(() => new ArrayIterator([
         new Map<string, RDF.Term>([
-          ['template', DF.namedNode('urn:template')],
-          ['pattern', DF.literal('.*')],
-          ['text', DF.literal('<p>example</p>')],
-          ['target', DF.namedNode('https://localhost/sparql')]
+          ['template', dataFactory.namedNode('urn:template')],
+          ['pattern', dataFactory.literal('.*')],
+          ['text', dataFactory.literal('<p>example</p>')],
+          ['target', dataFactory.namedNode('https://localhost/sparql')]
         ])
       ]))
     } as unknown as IQueryEngine;
     processor = new TermTemplateProcessor({
       queryEngine,
-      templateSources
+      dataFactory,
+      algebraFactory,
+      templateSources,
+      batchWindow: 200,
+      queryIdentifierVariable: 'query'
     });
+  });
+
+  afterEach(() => {
+    vi.resetAllMocks();
+    vi.restoreAllMocks();
   });
 
   describe('loadTemplates', () => {
@@ -70,29 +82,94 @@ describe('TermTemplateProcessor', () => {
   });
 
   describe('bindingsToRecords', () => {
-    it('converts a bindings stream to a record', async () => {
-      const bindingsStream = new ArrayIterator([
+    it('converts a bindings array to a record array', () => {
+      const bindings = [
         new Map<RDF.Variable, RDF.Term>([
-          [DF.variable('var1'), DF.literal('value1')]
+          [dataFactory.variable('var1'), dataFactory.literal('value1')]
         ]),
         new Map<RDF.Variable, RDF.Term>([
-          [DF.variable('var1'), DF.namedNode('ex:value2')]
+          [dataFactory.variable('var1'), dataFactory.namedNode('ex:value2')]
         ])
-      ]) as unknown as BindingsStream;
-      await expect(TermTemplateProcessor.bindingsToRecords(bindingsStream)).resolves.toEqual([
-        { var1: DF.literal('value1') },
-        { var1: DF.namedNode('ex:value2') }
+      ] as unknown as RDF.Bindings[];
+      expect(TermTemplateProcessor.bindingsToRecords(bindings)).toEqual([
+        { var1: dataFactory.literal('value1') },
+        { var1: dataFactory.namedNode('ex:value2') }
       ]);
+    });
+  });
+
+  describe('parseOperation', () => {
+    it('parses an operation', () => {
+      const query = '';
+      expect(TermTemplateProcessor.parseOperation(query)).toEqual({ type: 'nop' });
+    });
+  });
+
+  describe('serializeOperation', () => {
+    it('serializes an operation', () => {
+      const operation = algebraFactory.createNop();
+      expect(TermTemplateProcessor.serializeOperation(operation)).toBe('');
     });
   });
 
   describe('addBindingsToQuery', () => {
     it('adds bindings as values clause to the query', () => {
       const query = 'SELECT ?s WHERE { ?s ?p ?o }';
-      const bindings = [{ o: DF.namedNode('ex:o') }];
-      expect(TermTemplateProcessor.addBindingsToQuery(query, bindings)).toBe(
+      const bindings = [{ o: dataFactory.namedNode('ex:o') }];
+      expect(processor.addBindings(query, bindings)).toBe(
         'SELECT ?s WHERE {\n  ?s ?p ?o .\n  VALUES ?o {\n    <ex:o>\n  }\n}'
       );
+    });
+  });
+
+  describe('visualiseTerm', () => {
+    it('returns direct serialization without applicable templates', async () => {
+      vi.spyOn(processor, 'findApplicableTemplates').mockReturnValue([]);
+      await expect(processor.visualiseTerm({ value: 'abc' } as unknown as RDF.Term)).resolves.toBe(
+        '<pre>{\n  "value": "abc"\n}</pre>'
+      );
+      expect(processor.findApplicableTemplates).toHaveBeenCalledTimes(1);
+    });
+
+    it('visualises the term recursively', async () => {
+      vi.spyOn(processor, 'findApplicableTemplates').mockReturnValue([{ text: 'template content' } as unknown as TermTemplate]);
+      vi.spyOn(processor, 'askBatched').mockResolvedValue('ask result' as unknown as boolean);
+      vi.spyOn(processor, 'selectBatched').mockResolvedValue(['select bindings' as unknown as RDF.Bindings]);
+      vi.spyOn(TermTemplateProcessor, 'bindingsToRecords').mockResolvedValue('select result' as unknown as Record<string, RDF.NamedNode | RDF.Literal>[]);
+      vi.spyOn(TermTemplateProcessor.eta, 'renderStringAsync').mockImplementation(async (template: string, data: object): Promise<string> => {
+        expect(template).toBe('template content');
+        expect((data as ITermTemplateContext).term).toBeOneOf(['term to visualise', 'recursive term']);
+        if ((data as ITermTemplateContext).term as unknown as string === 'term to visualise') {
+          await expect((data as ITermTemplateContext).queryAsk('', [])).resolves.toBe('ask result');
+          await expect((data as ITermTemplateContext).querySelect('', [])).resolves.toBe('select result');
+          await expect((data as ITermTemplateContext).visualiseTerm('recursive term' as unknown as RDF.Term)).resolves.toBe('recursive output html');
+          return 'output html';
+        } else {
+          return 'recursive output html';
+        }
+      });
+      await expect(processor.visualiseTerm('term to visualise' as unknown as RDF.Term)).resolves.toBe('output html');
+      expect(processor.findApplicableTemplates).toHaveBeenCalledTimes(2);
+      expect(processor.askBatched).toHaveBeenCalledTimes(1);
+      expect(processor.selectBatched).toHaveBeenCalledTimes(1);
+      expect(TermTemplateProcessor.bindingsToRecords).toHaveBeenCalledTimes(1);
+      expect(TermTemplateProcessor.eta.renderStringAsync).toHaveBeenCalledTimes(2);
+    });
+
+    it('visualises errors during template rendering', async () => {
+      vi.spyOn(processor, 'findApplicableTemplates').mockReturnValue([{ text: 'template content' } as unknown as TermTemplate]);
+      vi.spyOn(processor, 'askBatched').mockResolvedValue('ask result' as unknown as boolean);
+      vi.spyOn(processor, 'selectBatched').mockResolvedValue(['select bindings' as unknown as RDF.Bindings]);
+      vi.spyOn(TermTemplateProcessor, 'bindingsToRecords').mockResolvedValue('select result' as unknown as Record<string, RDF.NamedNode | RDF.Literal>[]);
+      vi.spyOn(TermTemplateProcessor.eta, 'renderStringAsync').mockRejectedValue(new Error('Template rendering error'));
+      await expect(processor.visualiseTerm('term to visualise' as unknown as RDF.Term)).resolves.toBe(
+        '<pre class="error">Error: Template rendering error</pre>'
+      );
+      expect(processor.findApplicableTemplates).toHaveBeenCalledTimes(1);
+      expect(processor.askBatched).not.toHaveBeenCalled();
+      expect(processor.selectBatched).not.toHaveBeenCalled();
+      expect(TermTemplateProcessor.bindingsToRecords).not.toHaveBeenCalled();
+      expect(TermTemplateProcessor.eta.renderStringAsync).toHaveBeenCalledTimes(1);
     });
   });
 });
